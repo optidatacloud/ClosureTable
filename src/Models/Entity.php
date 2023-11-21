5a15d77ebb1e918a203571cbd72325d1a5bcb36a
Fernando-Magnabosco <fernandomagnabosco1@gmail.com>
2023-11-21 17:02:16 -0300

    fix: updated at

--- src/Models/Entity.php
+++ src/Models/Entity.php
@@ -304,6 +304,7 @@ class Entity extends Eloquent implements EntityInterface
         });
 
         static::saved(static function (Entity $entity) {
+            $entity->timestamps = false;
             $parentIdChanged = $entity->isDirty($entity->getParentIdColumn());
 
             if ($parentIdChanged || $entity->isDirty($entity->getPositionColumn())) {
@@ -320,6 +321,7 @@ class Entity extends Eloquent implements EntityInterface
             if ($parentIdChanged) {
                 $entity->closure->moveNodeTo($entity->parent_id);
             }
+            $entity->timestamps = true;
         });
     }
 
